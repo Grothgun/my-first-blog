4514c47d04f2ac6c62bef25090694e0db532e2b5
Grothgun <patryk-blumensztajn@wp.pl>
2017-10-04 19:58:06 +0200

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Post, Comment
+from .models import Post, Comment
 
 class CommentAdmin(admin.ModelAdmin):
 	list_display = ('name','email','post','created','active')
