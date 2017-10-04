ffedd452a5f6b2fdda1d3ad9fd2110e475521865
Grothgun <patryk-blumensztajn@wp.pl>
2017-10-04 19:30:27 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 	objects = models.Manager()
 	published = PublishedManager()
-        tags =TaggableManager()
+	tags =TaggableManager()
 	def get_absolute_url(self):
 		return reverse('blog:post_detail',args=[self.publish.year,
 												self.publish.strftime('%m'),
